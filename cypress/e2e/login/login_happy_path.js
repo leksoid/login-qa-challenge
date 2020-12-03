3f9101ec20fb155f860a8b23931555d8ff7dcd55
Alex Taran <aliaksei.taran@gmail.com>
2020-12-02 19:59:30 -0500

    Add viewport array so to run tests against different device sizes

--- cypress/e2e/login/login_happy_path.js
+++ cypress/e2e/login/login_happy_path.js
@@ -1,22 +1,34 @@
 import { loginWith, logout } from '../../support/login-logout-util'
 
+// test for 3 different device sizes: mobile, tablet, dekstop
+const sizes = ['iphone-8', 'ipad-2', 'macbook-11']
+
 describe('Login Page - Happy Path', () => {
+
+  sizes.forEach((size) => {
+
     beforeEach(() => {
+      // setting viewport and navigating to Login Page for each test
+      cy.viewport(size)
       cy.visit('/login').contains('Login Page')
       cy.log("Navigating to login page before each test")
     })
 
-    it('Existing user with valid credentials should log in', () => {
+    it(`Existing user with valid credentials should log in. Device size: ${size}`, () => {
       loginWith({ username: 'tomsmith', password: 'SuperSecretPassword!' })
       cy.get('.flash.success').should('include.text', 'You logged into a secure area!')
       cy.get('#login').should('not.exist');
+      // check that after page refresh, user still logged in
+      cy.reload()
+          .get('a[href="/logout"').should('exist')
     })
 
-    it('Logged user should be able to logout', () => {
+    it(`Logged user should be able to logout. Device size: ${size}`, () => {
       loginWith({ username: 'tomsmith', password: 'SuperSecretPassword!' })
       cy.get('.flash.success').should('include.text', 'You logged into a secure area!')
       logout()
       cy.get('.flash.success').should('include.text', 'You logged out of the secure area!')
       cy.get('#login').should('be.visible');
     })
+  })
 })
--- cypress/e2e/login/login_negative_tests.js
+++ cypress/e2e/login/login_negative_tests.js
@@ -1,30 +1,39 @@
 import { loginWith } from '../../support/login-logout-util'
 
-describe('Login Page - Negative Tests', () => {
-    beforeEach(() => {
-        cy.visit('/login').contains('Login Page')
-        cy.log("Navigating to login page before each test")
-      })
+// test for 3 different device sizes: mobile, tablet, dekstop
+const sizes = ['iphone-8', 'ipad-2', 'macbook-11']
 
-    it('Error message displayed if login form submitted is blank', () => {
+describe('Login Page - Negative Tests', () => {
+
+  sizes.forEach((size) => {
+    
+    beforeEach(() => {
+      // setting viewport and navigating to Login Page for each test
+      cy.viewport(size)
+      cy.visit('/login').contains('Login Page')
+      cy.log("Navigating to login page before each test")
+    })
+
+    it(`Error message displayed if login form submitted is blank. Device size: ${size}`, () => {
       cy.get('button[type="submit"').click()
         .get('.flash.error').should('be.visible')
     })
 
-    it('Error message displayed if password missing', () => {
+    it(`Error message displayed if password missing. Device size: ${size}`, () => {
       loginWith({ username: 'tomsmith', password: '' })
       cy.get('.flash.error').should('include.text', 'Your password is invalid!')
     })
 
-    it('Existing user should not login with invalid password', () => {
+    it(`Existing user should not login with invalid password. Device size: ${size}`, () => {
       loginWith({ username: 'tomsmith', password: 'invalidPassword' })
       cy.get('.flash.error').should('be.visible')
         .should('include.text', 'Your password is invalid!')
     })
 
-    it('Non-Existing user should not be able to login', () => {
+    it(`Non-Existing user should not be able to login. Device size: ${size}`, () => {
       loginWith({ username: 'wwwwrrrrr', password: 'qwerty1234' })
       cy.get('.flash.error').should('be.visible')
         .should('include.text', 'Your username is invalid!')
     })
+  })
 })
